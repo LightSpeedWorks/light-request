a0753e062c083543e7a8a35a69e5eaf3873cae48
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2016-11-04 16:40:56 +0900

    light-request v0.0.1 support callback, promise and thunk

--- light-request.js
+++ light-request.js
@@ -50,76 +50,134 @@
 
 	return request;
 
-	function requestXHR(method, options, url, data) {
-		if (typeof options === 'string')
-			data = url, url = options, options = {};
+	function requestXHR(method, options, url, data, cb) {
+		var i = 0, a = arguments, n = arguments.length;
+		cb = typeof a[n - 1] === 'function' ? a[--n] : promiseThunkCallback();
+		method = i < n ? a[i++] : undefined;
+		options = i < n && typeof a[i] === 'object' && a[i++] || {};
+		url = i < n ? a[i++] : undefined;
+		data = i < n ? a[i++] : undefined;
+
 		var start = new Date;
-		return new Promise(function (resolve, reject) {
-			var xhr = new XMLHttpRequest;
-			xhr.onreadystatechange = function () {
-				//console.log('time:', (new Date - start) / 1000, 'sec, State:',
-				//	deps.readyState[xhr.readyState]);
-				if (xhr.readyState === 4) {
-					if (xhr.status < 400) {
-						var res = {statusCode: xhr.status,
-							statusMessage: xhr.statusText,
-							body: xhr.response};
-						//xhr.getResponseHeader()
-						res.headers = xhr.getAllResponseHeaders();
-						res.responseURL = xhr.responseURL;
-						res.type = xhr.responseType;
-						try {
-							if (typeof res.body === 'string')
-								res.body = JSON.parse(res.body);
-						} catch (e) {}
-						resolve(res);
-					}
-					else reject(new Error('Status ' + xhr.status + ': ' + xhr.statusText));
+		var xhr = new XMLHttpRequest;
+		xhr.onreadystatechange = function () {
+			//console.log('time:', (new Date - start) / 1000, 'sec, State:',
+			//	deps.readyState[xhr.readyState]);
+			if (xhr.readyState === 4) {
+				if (xhr.status < 400) {
+					var res = {statusCode: xhr.status,
+						statusMessage: xhr.statusText,
+						body: xhr.response};
+					//xhr.getResponseHeader()
+					res.headers = xhr.getAllResponseHeaders();
+					res.responseURL = xhr.responseURL;
+					res.type = xhr.responseType;
+					try {
+						if (typeof res.body === 'string')
+							res.body = JSON.parse(res.body);
+					} catch (e) {}
+					cb(null, res);
 				}
+				else cb(new Error('Status ' + xhr.status + ': ' + xhr.statusText),
+						{statusCode: xhr.status, statusMessage: xhr.statusText});
 			}
-			xhr.onerror = reject;
-			xhr.open(method, url, true);
-			if (options && options.headers)
-				for (var i in options.headers)
-					try { xhr.setRequestHeader(i, options.headers[i]); }
-					catch (e) { console.error(e); }
-			if (typeof data !== 'undefined') {
-				xhr.setRequestHeader('Content-Type', 'application/json');
-				xhr.send(JSON.stringify(data));
-			}
-			else xhr.send();
-		}); // new Promise
+		}
+		xhr.onerror = cb;
+		xhr.open(method, url, true);
+		if (options && options.headers)
+			for (var i in options.headers)
+				try { xhr.setRequestHeader(i, options.headers[i]); }
+				catch (e) { console.error(e); }
+		if (typeof data !== 'undefined') {
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.send(JSON.stringify(data));
+		}
+		else xhr.send();
+		return cb.promiseThunk;
 	} // requestXHR
 
-	function requestHTTP(method, options, url, data) {
-		if (typeof options === 'string')
-			data = url, url = options, options = {};
-		return new Promise(function (resolve, reject) {
-			var opts = deps.parseURL(url);
-			opts.method = method;
-			opts.headers = {};
-			if (options && options.headers)
-				for (var i in options.headers)
-					opts.headers[i] = options.headers[i];
+	function requestHTTP(method, options, url, data, cb) {
+		var i = 0, a = arguments, n = arguments.length;
+		cb = typeof a[n - 1] === 'function' ? a[--n] : promiseThunkCallback();
+		method = i < n ? a[i++] : undefined;
+		options = i < n && typeof a[i] === 'object' && a[i++] || {};
+		url = i < n ? a[i++] : undefined;
+		data = i < n ? a[i++] : undefined;
 
-			if (typeof data !== 'undefined') {
-				opts.headers['Content-Type'] = 'application/json';
-				data = JSON.stringify(data);
-			}
+		var opts = deps.parseURL(url);
+		opts.method = method;
+		opts.headers = {};
+		if (options && options.headers)
+			for (var i in options.headers)
+				opts.headers[i] = options.headers[i];
 
-			var req = deps[opts.protocol].request(opts, function (res) {
-				var bufs = [];
-				res.on('data', function (buf) { bufs.push(buf); });
-				res.on('end', function () {
-					res.body = Buffer.concat(bufs).toString();
-					try { res.body = JSON.parse(res.body); } catch (e) {}
-					resolve(res);
-				});
+		if (typeof data !== 'undefined') {
+			opts.headers['Content-Type'] = 'application/json';
+			data = JSON.stringify(data);
+		}
+
+		var req = deps[opts.protocol].request(opts, function (res) {
+			var bufs = [];
+			res.on('data', function (buf) { bufs.push(buf); });
+			res.on('end', function () {
+				res.body = Buffer.concat(bufs).toString();
+				try { res.body = JSON.parse(res.body); } catch (e) {}
+				if (res.statusCode < 400)
+					cb(null, res);
+				else cb(new Error('Status ' + res.statusCode + ': ' + res.statusMessage), res);
 			});
-			if (typeof data !== 'undefined') req.write(data);
-			req.end();
-		}); // new Promise
+		});
+		if (typeof data !== 'undefined') req.write(data);
+		req.end();
+		return cb.promiseThunk;
 	} // requestHTTP
+
+	function promiseThunkCallback() {
+		var promise, resolve, reject, pending = true;
+		var ctx, results, cbs = [];
+
+		function setup(res, rej) { resolve = res; reject = rej; }
+
+		function thunk(cb) {
+			if (typeof cb === 'function')
+				cbs.push(cb);
+			if (results !== undefined)
+				callback.apply(ctx, results);
+		}
+
+		thunk.then = function then(res, rej) {
+			if (promise === undefined)
+				promise = new Promise(setup);
+			if (results !== undefined)
+				callback.apply(ctx, results);
+			return promise.then(res, rej);
+		};
+
+		thunk['catch'] = function caught(rej) {
+			if (promise === undefined)
+				promise = new Promise(setup);
+			if (results !== undefined)
+				callback.apply(ctx, results);
+			return promise['catch'](rej)
+		};
+
+		function callback(err, val) {
+			if (results === undefined)
+				ctx = this, results = arguments;
+
+			cbs.forEach(function (cb) { cb.apply(ctx, results); });
+			cbs = [];
+
+			if (pending && promise !== undefined) {
+				if (results[0]) reject(results[0]);
+				else resolve(results[1]);
+				pending = false;
+			}
+		}
+
+		callback.promiseThunk = thunk;
+		return callback;
+	} // promiseThunkCallback
 
 } ();
 
